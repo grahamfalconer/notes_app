5eaabb6c14b864a00a9ce9eb02bcbfe521fa67f4
Harry Turnbull <hturnbull@live.co.uk>
2020-04-27 16:57:20 +0100

    linted

--- note.js
+++ note.js
@@ -1,5 +1,5 @@
-function Note(title, description){
-    this.title = title
-    this.description = description
-    this.shortDescription = description.substring(0, 20)
+function Note(title, description) {
+  this.title = title;
+  this.description = description;
+  this.shortDescription = description.substring(0, 20);
 }
--- noteTest.js
+++ noteTest.js
@@ -1,9 +1,12 @@
-it("can create a note with a description and title", function() {
-    var note = new Note("Testing title", "Test Description");
-    assert.isEqual(note.title, "Testing title");
-    assert.isEqual(note.description, "Test Description");
-})
-it("will return the first 20 characters if description exceeds 20 characters(nice test bro)", function() {
-    var note = new Note("Testing title", "Lorum Ipsum is a lady who was born in Croatia, her first haircut was by her mother beth, her second by her brother Bilo");
-    assert.isEqual(note.shortDescription, "Lorum Ipsum is a lad");
-})
+it("can create a note with a description and title", function () {
+  var note = new Note("Testing title", "Test Description");
+  assert.isEqual(note.title, "Testing title");
+  assert.isEqual(note.description, "Test Description");
+});
+it("will return the first 20 characters if description exceeds 20 characters(nice test bro)", function () {
+  var note = new Note(
+    "Testing title",
+    "Lorum Ipsum is a lady who was born in Croatia, her first haircut was by her mother beth, her second by her brother Bilo"
+  );
+  assert.isEqual(note.shortDescription, "Lorum Ipsum is a lad");
+});
