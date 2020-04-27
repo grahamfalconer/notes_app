a3d48abd0857413b4137c13444d1a801f7b529cb
Harry Turnbull <hturnbull@live.co.uk>
2020-04-27 16:55:09 +0100

    Test for notepad adding a note with newnote

---
+++ notepadTest.js
@@ -0,0 +1,9 @@
+it('can create a new note and add it to the stored notes', () => {
+
+  let notePad = new NotePad()
+
+  notePad.newNote("Title", "Description")
+
+  assert.isEqual(notePad.notes[0].title, "Title")
+
+});
